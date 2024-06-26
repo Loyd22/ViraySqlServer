d3ede144a919e89439b16d2f4d5217203811a94d
Viray, John Loyd <115643874+Loyd22@users.noreply.github.com>
2024-06-26 18:56:22 +0800

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,16 @@
+﻿using digiBookModel;
+using System.Reflection;
+
+using System.Collections.Generic;
+using digiBookModel;
+
+namespace digiBookDataLayer
+{
+    public class Data
+    {
+        public static List<bookss> book()
+        {
+            return sqldbdata.GetBooksFromDatabase();
+        }
+    }
+}
---
+++ sqldbdata.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using digiBookModel;
+
+namespace digiBookDataLayer
+{
+    public class sqldbdata
+    {
+        private static string connectionString = "Data Source=YEL\\SQLEXPRESS;Initial Catalog=Digibook;Integrated Security=True;";
+
+        public static List<bookss> GetBooksFromDatabase()
+        {
+            List<bookss> books = new List<bookss>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT Title, Author, Summary FROM Books";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        bookss book = new bookss
+                        {
+                            title = reader["Title"].ToString(),
+                            author = reader["Author"].ToString(),
+                            summary = reader["Summary"].ToString()
+                        };
+                        books.Add(book);
+                    }
+                }
+            }
+
+            return books;
+        }
+    }
+}
